586037806184d67bedad0a80eb27009b0142dae8
addwon <addwon@gmail.com>
2018-02-17 17:02:39 -0600

    Commit 7

---
+++ src/main/java/com/week4challange/roboresumewithsecurity/UserDataRepository.java
@@ -0,0 +1,6 @@
+package com.week4challange.roboresumewithsecurity;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserDataRepository extends CrudRepository<UserData,Long> {
+}
